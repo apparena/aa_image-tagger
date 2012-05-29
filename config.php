691fe1353abdb7ca65a38fd37619ba84ca60f33b
guntram <guntrampollock@arcor.de>
2012-05-29 18:29:44 +0200

    gitignore only working on push?!

--- config.php
+++ config.php
@@ -5,25 +5,24 @@
 $config_data=array(
    // Database settings for facebook application
    'database_host' => "localhost",
-   'database_name' => "dbtable",
-   'database_user' => "dbuser",
-   'database_pass' => "dbpass",
+   'database_name' => "apps_tagging_v10",
+   'database_user' => "aa_tagging",
+   'database_pass' => "Denofe98",
 
 
    // Setup App-Arena App-Manager Connection
-   'aa_app_id' => aa_id,
-   'aa_app_secret' => "aa_secret",
+   'aa_app_id' => 157,
+   'aa_app_secret' => "d346590ff4573aae73d52cfad6c48d3d",
 
    'debugMode' => true
-   
 );
 
    $database_host = "localhost";
-   $database_name = "dbtable";
-   $database_user = "dbuser";
-   $database_pass = "dbpass";
+   $database_name = "apps_tagging_v10";
+   $database_user = "aa_tagging";
+   $database_pass = "Denofe98";
 
-   $aa_app_id = aa_id;
-   $aa_app_secret = "aa_secret";
+   $aa_app_id = 157;
+   $aa_app_secret = "d346590ff4573aae73d52cfad6c48d3d";
    
 ?>
